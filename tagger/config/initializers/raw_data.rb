a73ff2153c93431266227d4987d25ab062623c26
Interfish <330508653@qq.com>
2019-01-04 18:20:43 +0800

    Init

--- tagger/config/initializers/raw_data.rb
+++
@@ -1,7 +0,0 @@
-RAW_DATA = Dir.entries(Rails.root.join('..', 'raw_data')).reject { |e| %w{. ..}.include?(e) }.sort do |a, b|
-  if a.split('_')[0].to_i == b.split('_')[0].to_i
-    a <=> b
-  else
-    a.split('_')[0].to_i - b.split('_')[0].to_i
-  end
-end.freeze
