14fb688d41f5fbc1b21ca286ce201184873330c1
Interfish <330508653@qq.com>
2019-01-09 17:49:11 +0800

    Update: add some scope

--- app/models/code_snippet.rb
+++ app/models/code_snippet.rb
@@ -1,4 +1,8 @@
 class CodeSnippet < ApplicationRecord
     serialize :indices, Array
     scope :not_classified,  -> { where(status: nil) }
+    scope :unsure, -> { where(status: 2) }
+    scope :leaked, -> { where(status: 1) }
+    scope :normal, -> { where(status: 0) }
+    scope :critical_content, -> { map { |snippet| snippet.indices.map { |indice| snippet.content.slice(indice.first..indice.last) } }.flatten }
 end
