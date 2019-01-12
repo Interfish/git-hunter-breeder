35772ce28a7786aa538c58031b85d21b97be1a95
Interfish <330508653@qq.com>
2019-01-12 16:55:43 +0800

    Fix: minor fix

--- app/controllers/tagger_controller.rb
+++ app/controllers/tagger_controller.rb
@@ -5,7 +5,7 @@ class TaggerController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @index = params[:index].present? ? params[:index].to_i : CodeSnippet.unclassified.first.id
+    @index = params[:index].present? ? params[:index].to_i : (CodeSnippet.unclassified.first.id || CodeSnippet.last.id)
     @index = [[1, @index].max, CodeSnippet.last.id].min
     @snippet = CodeSnippet.find(@index)
     content = @snippet.content
