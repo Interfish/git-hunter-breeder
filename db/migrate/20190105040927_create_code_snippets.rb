bd2e5c677cffe9fd3cfdcb605fd55cd17fb35be1
Interfish <330508653@qq.com>
2019-01-16 18:33:04 +0800

    add hint for snippet status

--- db/migrate/20190105040927_create_code_snippets.rb
+++ db/migrate/20190105040927_create_code_snippets.rb
@@ -5,7 +5,7 @@ class CreateCodeSnippets < ActiveRecord::Migration[5.2]
       t.string :file_name, comment: 'file full path'
       t.text :content, comment: 'code snippet content'
       t.string :indices, comment: 'indices for leaked password'
-      t.integer :status, default: nil, comment: 'status of a snippet. nil for not classified, 0 for good snippet, 1 for leaked snippet, 2 for unsure snippet.'
+      t.integer :status, default: nil, comment: 'status of a snippet. nil for not classified, 0 for good snippet, 1 for leaked snippet, 2 for unsure snippet, 3 for ignore '
       t.index :key, unique: true
       t.timestamps
     end
